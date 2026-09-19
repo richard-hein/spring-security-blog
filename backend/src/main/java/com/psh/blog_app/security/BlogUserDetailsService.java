f6487928e59b4dc8f93825c1021a2852c757a67b
Richard-Hein <pyaesone.dev.web@gmail.com>
2026-09-19 20:36:38 +0630

    Security init

--- backend/src/main/java/com/psh/blog_app/security/BlogUserDetailsService.java
+++ backend/src/main/java/com/psh/blog_app/security/BlogUserDetailsService.java
@@ -1,4 +1,21 @@
 package com.psh.blog_app.security;
 
-public class BlogUserDetailsService {
+import com.psh.blog_app.domain.entities.User;
+import com.psh.blog_app.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class BlogUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email).orElseThrow(()-> new UsernameNotFoundException("User not found with this email: "+ email));
+        return new BlogUserDetails(user);
+
+    }
 }
